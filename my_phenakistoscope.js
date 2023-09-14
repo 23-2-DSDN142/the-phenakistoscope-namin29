dc461d790b9067bfbea07a857611ebf17a14684e
namin29 <140354195+namin29@users.noreply.github.com>
2023-09-14 12:07:01 +1200

    iefijaeifhj

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -6,27 +6,45 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+  pScope.load_image("greensquare" , "png");
 }
 
 function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
+  var layer1 = new PLayer(greenSquares);
+  layer1.mode( SWIRL(2) );
+  layer1.set_boundary( 500, 1000 );
+
   var layer1 = new PLayer(faces);
   layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 500, 1000 );
+  layer1.set_boundary( 50, 1100 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
   layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
+function greenSquares(x, y, animation, pScope){
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
+ // scale(0.5);
+ // pScope.draw_image("greensquare",x,y);
+  
+
+}
+
+function faces(x, y, animation, pScope){
+
+  rotate(45);
+  scale(animation.frame*2.5);
+  noStroke();
+  const c = color(0, 126, 255, 50);
+  fill(c);
+  rect(15, 15, 70, 70);
+  // Sets 'alphaValue' to 102.
+  const alphaValue = alpha(c);
+  fill(alphaValue);
   
 
 }
@@ -38,10 +56,12 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+ // fill(66, 135, 245)
+ // arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
   fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+ // rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+
+ triangle(30, 75, 58, 20, 86, 75);
 
 }
